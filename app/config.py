fb2e3cce6dd501de020847587944b4825fd46ea1
lilian-2021 <lilian.kigunda@student.moringaschool.com>
2022-02-12 17:05:25 +0300

    added views

---
+++ app/config.py
@@ -0,0 +1,66 @@
+# import secrets
+
+# class Base:
+#     FLASK_APP= "main.py"
+#     SQLALCHEMY_TRACK_MODIFICATION=False
+#     SECRET_KEY=secrets.token_hex(16)
+    
+    
+    
+# class Development(Base):
+#     FLASK_ENV = "development"
+#     # DATABASE = os.environ.get('DATABASE')
+#     DATABASE = ""
+#     POSTGRES_USER= ""
+#     POSTGRES_PASSWORD=""
+#     SQLALCHEMY_DATABASE_URI="postgresql+psycopg2://moringa:1234@localhost/pitchess"
+    
+    
+    
+# class staging(Base):
+#     DATABASE = ""
+#     POSTGRES_USER= ""
+#     POSTGRES_PASSWORD=""
+#     SQLALCHEMY_DATABASE_URI=""
+
+    
+# class production(Base):
+#         DATABASE = ""
+#         POSTGRES_USER= ""
+#         POSTGRES_PASSWORD=""
+#         SQLALCHEMY_DATABASE_URI=""
+
+  
+import os
+os.urandom(24)
+
+class Config:
+    SECRET_KEY = 'mysecretket'
+    SQLALCHEMY_DATABASE_URI ='postgresql+psycopg2://moringa:lilian@localhost/apostgre'
+
+    @staticmethod
+    def init_app(app):
+        pass
+
+
+class ProdConfig(Config):
+    '''
+    Production  configuration child class
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+    # SQLALCHEMY_DATABASE_URI =os.environ.get('SQLALCHEMY_DATABASE_URI')
+    SQLALCHEMY_DATABASE_URI ='postgresql+psycopg2://moringa:lilian@localhost/apostgre'
+
+
+class DevConfig(Config):
+    '''
+    Development  configuration child class
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+    DEBUG = True
+
+
+
+        
--- models/comments.py
+++
@@ -1,6 +0,0 @@
-from main import db
-
-class Comments(db.Model):
-    __tablename__ = 'comments'
-    id = db.Column(db.Integer, primary_key=True)
-    comment = db.Column(db.Text, nullable=False,)
